9bca5ce41a0c6dc7cc8cb9a3c4fe04c440557f78
Sabelo Hlatshwayo <evidence.hlatshwayo@gmail.com>
2023-04-04 12:38:41 +0200

    Add the JavaScript code for selecting the form, the form inputs, and the submit button

---
+++ script.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const form = document.querySelector(".form");
+const username = document.querySelector("#username");
+const email = document.querySelector("#email");
+const firstPassword = document.querySelector("#firstPassword");
+const secondPassword = document.querySelector("#secondPassword");
+const btnSubmit = document.querySelector(".btnSubmit");
