3964dc380ebcca608aaeddf60e44f250dbeea103
Sabelo Hlatshwayo <evidence.hlatshwayo@gmail.com>
2023-04-07 19:14:48 +0200

    Create a separate module for the DOM selectors of the form elements

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,13 @@
 "use strict";
 
-const form = document.querySelector(".form");
-const username = document.querySelector("#username");
-const email = document.querySelector("#email");
-const firstPassword = document.querySelector("#firstPassword");
-const secondPassword = document.querySelector("#secondPassword");
-const btnSubmit = document.querySelector(".btnSubmit");
-const inputs = document.querySelectorAll(".input");
+import {
+    form,
+    username,
+    email,
+    firstPassword,
+    secondPassword,
+    inputs,
+} from "./selectors.js";
 
 const minUsernameLength = 3;
 const maxUsernameLength = 15;
---
+++ scripts/selectors.js
@@ -0,0 +1,6 @@
+export const form = document.querySelector(".form");
+export const username = document.querySelector("#username");
+export const email = document.querySelector("#email");
+export const firstPassword = document.querySelector("#firstPassword");
+export const secondPassword = document.querySelector("#secondPassword");
+export const inputs = document.querySelectorAll(".input");
